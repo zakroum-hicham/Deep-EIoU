98bd64a12cef2de660a176312b6ede6e09debc86
Hicham Zakroum <hicham.zakroum2@gmail.com>
2025-02-25 17:27:06 +0000

    fix module not found error

--- Deep-EIoU/reid/torchreid/utils/feature_extractor.py
+++ Deep-EIoU/reid/torchreid/utils/feature_extractor.py
@@ -4,10 +4,10 @@ import torch
 import torchvision.transforms as T
 from PIL import Image
 
-from torchreid.utils import (
+from reid.torchreid.utils import (
     check_isfile, load_pretrained_weights, compute_model_complexity
 )
-from torchreid.models import build_model
+from reid.torchreid.models import build_model
 
 
 class FeatureExtractor(object):
